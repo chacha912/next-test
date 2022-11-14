bf6cb316509dbfb87d0a81f37103f844cdfb5030
Yourim Cha <yourim.cha@navercorp.com>
2022-11-14 14:41:21 +0900

    base test

--- next.config.js
+++ next.config.js
@@ -15,7 +15,6 @@ const nextConfig = {
     nextImageExportOptimizer_generateAndUseBlurImages: true,
   },
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH + '/' || '',
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/i,
